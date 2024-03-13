e000c045f353e29d79e4ffd8a3a47ad112f26435
zhenganna <zan272905918@163.com>
2024-03-13 10:38:28 +0800

    source map & devserver

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development', // "production" \ "development" \ "none"
+  devtool: 'eval', // "eval" \ "source-map" \ "hidden-source-map" \ "inline-source-map" \
   entry: {
     app: './src/index.js'
     // app2: './src/index2.js'
@@ -18,9 +19,20 @@ module.exports = {
     // 资源放置在 CDN 上
     // publicPath: 'www.xxx.com'
   },
+  // 原理：使用express起一个node服务，并使用webpackdevmiddleware
   devServer: {
     compress: true,
-    port: 9000
+    port: 9000,
+    // 热更新不刷新页面会保留状态局部更新
+    // js代码强制更新，不刷 新页面不保留状态
+    hot: true, // 主要应用于资源文件
+    // 解决开发模式下的跨域问题
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
+        pathRewrite: { '^/api': '' }
+      }
+    }
   },
   resolve: {
     // 别名，提供路径的简写
